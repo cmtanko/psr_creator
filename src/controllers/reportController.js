0abb68904ab10a71e6032d9e6bc01f4737a53f48
Suchan Badyakar <suchanbadyakar@lftechnology.com>
2017-08-15 14:39:35 +0545

    PSRG-007 -m Cleanups -t 1 hr -s Completed

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -53,7 +53,6 @@ var reportController = function (reportService, querystring) {
                 }
             }
         }, this);
-        console.log(JSON.stringify(query));
         res.render('reportView', {
             reportPage: 'Hello from report Page',
             repoDatas: 'repoDatas',
@@ -63,8 +62,13 @@ var reportController = function (reportService, querystring) {
             startDate: moment(query.from).format('MM/DD/YYYY'),
             endDate: moment(query.to).format('MM/DD/YYYY'),
             currentDate: moment(query.today).format('MM/DD/YYYY'),
+            entryStartDate: moment(query.from).format('YYYY-MM-DD'),
+            entryEndDate: moment(query.to).format('YYYY-MM-DD'),
             projectName: query.projectName,
             projectStatus:query.projectStatus,
+            projectToken: query.projectToken.slice(6, query.projectToken.length),
+            projectUser: query.projectUser,
+            isHidden: 'block',
             lastWeekIssues: _.sortBy(totalIssuesLastWeek, ['task_status', 'task_id']),
             thisWeekIssues: _.sortBy(totalIssuesThisWeek, ['task_status', 'task_id'])
         });
@@ -72,17 +76,16 @@ var reportController = function (reportService, querystring) {
 
     var getReport = function (req, res) {
         var query = req.query;
-        console.log(query);
         userInfo = {};
         userInfo['username'] = query['username'];
         userInfo['reponame'] = query['reponame'];
-        userInfo['projectStatus']  = query['projectStatus'];
-        userInfo['sha']  = query['sha'];
+        userInfo['projectStatus'] = query['projectStatus'];
+        userInfo['sha'] = query['sha'];
         userInfo['token'] = query['token'] === undefined || query['token'] === '' ? '' : 'Basic ' + query['token'];
 
-        query.username = undefined;
-        query.reponame = undefined;
-        query.token = undefined;
+        // query.username = undefined;
+        // query.reponame = undefined;
+        // query.token = undefined;
         queryParam = _.omitBy(query, _.isEmpty || _.isUndefined);
 
         let queryParamStr = {
@@ -90,15 +93,19 @@ var reportController = function (reportService, querystring) {
             to: new Date(query.until),
             today: new Date(),
             projectName: query.sha,
-            projectStatus: query.projectStatus
+            projectStatus: query.projectStatus,
+            projectToken: userInfo['token'],
+            projectUser: userInfo['username'],
+
         };
 
         //ASSIGNEE IS IMPORTANT
         if (!userInfo.username) {
             res.render('reportView', {
-                startDate: moment(queryParamStr.today).subtract(14,'d').format('YYYY-MM-DD'),
-                endDate: moment(queryParamStr.today).add(1,'d').format('YYYY-MM-DD'),
-                currentDate: moment(queryParamStr.today).format('YYYY-MM-DD'),
+                entryStartDate: moment(queryParamStr.today).subtract(14, 'd').format('YYYY-MM-DD'),
+                entryEndDate: moment(queryParamStr.today).add(1, 'd').format('YYYY-MM-DD'),
+                entryCurrentDate: moment(queryParamStr.today).format('YYYY-MM-DD'),
+                isHidden: 'none'
             });
             return;
         }
