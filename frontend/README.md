77f3428da197c3fb04ba514d701b17ae94010995
Suchan Badyakar <suchanbadyakar@lftechnology.com>
2017-08-22 11:16:52 +0545

    .

--- frontend/README.md
+++
@@ -1,2 +0,0 @@
-# psr_creator
-extract data from git hub repository and generate a daily/weekly or monthy report
--- frontend/src/app/index.components.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import footerModule from './components/footer/footer.module';
-import headerModule from './components/header/header.module';
-export default angular.module('index.components', [
-	footerModule.name, headerModule.name
-]);
